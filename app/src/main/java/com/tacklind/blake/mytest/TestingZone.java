23f83efaa8f71593cf0fe3c718de05dc0813da4d
Blake Tacklind <blake@tacklind.com>
2015-05-27 02:06:03 -0700

    IT QUERIES!

--- app/src/main/java/com/tacklind/blake/mytest/TestingZone.java
+++ app/src/main/java/com/tacklind/blake/mytest/TestingZone.java
@@ -1,6 +1,7 @@
 package com.tacklind.blake.mytest;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,9 +13,13 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 
 public class TestingZone extends ActionBarActivity {
@@ -30,39 +35,77 @@ public class TestingZone extends ActionBarActivity {
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 String TAG = "Test";
-                Log.v(TAG, "Stage 1");
+                //Log.v(TAG, "Stage 1");
                 try {
                     Class.forName("org.postgresql.Driver");
                 } catch (ClassNotFoundException e) {
                     e.printStackTrace();
                 }
-                Log.v(TAG, "Stage 2");
-                String  url ;
-                url = "jdbc:postgresql://sernity.isozill.com:5432/parcelexchange" +
-                        "?sslfactory=org.postgresql.ssl.NonValidatingFactory" +
-                        "&ssl=true";
-                Connection conn = null;
-                Log.v(TAG, "Stage 3");
-                try {
-                    conn = DriverManager. getConnection(url, "ParcelExchange", "Mabc0NDkYRf1yVyIfhRd");
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-                Log.v(TAG, "Stage 4");
+                //Log.v(TAG, "Stage 2");
 
-                if(conn!=null) {
-                    try {
-                        conn.close();
-                    } catch (SQLException e) {
-                        e.printStackTrace();
-                    }
-                }
-                Log.v(TAG, "Stage 5");
+                new GetData().execute();
             }
         });
 
     }
 
+
+    private class GetData extends AsyncTask<URL, Integer, Long>{
+        protected Long doInBackground(URL... urls) {
+            String  url ;
+            url = "jdbc:postgresql://serenity.isozilla.com:5432/parcelexchange" +
+                    "?sslfactory=org.postgresql.ssl.NonValidatingFactory" +
+                    "&ssl=true";
+            String TAG = "Test";
+            Connection conn = null;
+            //Log.v(TAG, "Stage 3");
+            try {
+                conn = DriverManager. getConnection(url, "parcelexchange", "Mabc0NDkYRf1yVyIfhRd");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            //Log.v(TAG, "Stage 4");
+
+            if(conn!=null) {
+
+                String sql;
+                sql = "SELECT username FROM users;";
+
+                Statement st = null;
+                try {
+                    st = conn.createStatement();
+                    ResultSet rs = null;
+                    rs = st.executeQuery(sql);
+
+                    while(rs.next()){
+                        String user = rs.getString("username");
+                        Log.v(TAG, user);
+                    }
+
+                    rs.close();
+                    st.close();
+
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+
+                //Log.v(TAG, "Stage 4.5");
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            //Log.v(TAG, "Stage 5");
+
+            return Long.valueOf(0);
+        }
+
+        protected void onPostExecute(Long result){
+            //Log.v("GetData", "Done Stuff");
+        }
+    }
 
 
     @Override
