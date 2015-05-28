b306568824f539921b5d3bb38825ebe6d276fe5a
Blake Tacklind <blake@tacklind.com>
2015-05-27 17:10:50 -0700

    Adding users to database works

--- app/src/main/java/com/tacklind/blake/mytest/TestingZone.java
+++ app/src/main/java/com/tacklind/blake/mytest/TestingZone.java
@@ -17,9 +17,11 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 
 public class TestingZone extends ActionBarActivity {
@@ -43,12 +45,173 @@ public class TestingZone extends ActionBarActivity {
                 }
                 //Log.v(TAG, "Stage 2");
 
-                new GetData().execute();
+                DataBaseWarper.getUsers();
+                DataBaseWarper.addUser("Joe");
             }
         });
 
     }
 
+
+    private static class DataBaseWarper{
+        final private static String url = "jdbc:postgresql://serenity.isozilla.com:5432/" +
+                "parcelexchange?sslfactory=org.postgresql.ssl.NonValidatingFactory" +
+                "&ssl=true";
+
+        public static void getUsers(){
+            new get().execute(new getStrings("users", "username"));
+
+            return;
+        }
+
+        public static void addUser(String name){
+            new add().execute(new addStrings("users", "username", name));
+        }
+
+        private static class getStrings{
+            public getStrings(String t, String c){
+                table = t;
+                columns = c;
+                extras = "";
+            }
+
+            public getStrings(String t, String c, String e){
+                this(t,c);
+                extras = e;
+            }
+
+            public String table;
+            public String columns;
+            public String extras;
+        }
+
+        private static class addStrings{
+            public addStrings(String t, String c, String v){
+                table = t;
+                column = c;
+                values = v;
+            }
+
+            public String table;
+            public String column;
+            public String values;
+        }
+
+        protected static class add extends AsyncTask<addStrings, Integer, Integer>{
+            Connection conn;
+            protected Integer doInBackground(addStrings... in){
+                conn = connectToDataBase();
+
+                for(addStrings g: in) {
+                    insertINTO(conn, g.table, g.column, g.values);
+                }
+
+                return 0;
+            }
+
+            protected void onPostExecute(Integer result){
+                getUsers();
+            }
+        }
+
+        protected static class get extends AsyncTask<getStrings, Integer, Integer>{
+            Connection conn;
+
+            protected Integer doInBackground(getStrings... in){
+                conn = connectToDataBase();
+                for(getStrings g: in) {
+                    for (String s : getFROM(conn, g.table, g.columns, g.extras)) {
+                        Log.v("getUsers", s);
+                    }
+                }
+
+                if (conn != null)
+                    try {
+                        conn.close();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+
+                return 0;
+            }
+        }
+
+
+
+        protected static void insertINTO (Connection conn, String table, String column, String values){
+            final String TAG = "insertINTO";
+
+            if (conn == null){
+                Log.e(TAG, "Connection was null");
+                return;
+            }
+
+            String sql;
+            sql = "INSERT INTO " + table + " (" + column + ") VALUES (?);";
+            try {
+                PreparedStatement st = conn.prepareStatement(sql);
+                st.setString(1, values);
+                st.executeUpdate();
+                st.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        protected static ArrayList<String> getFROM(Connection conn, String table, String column, String extra){
+            final String TAG = "getFROM";
+
+            if (conn == null){
+                Log.e(TAG, "Connection was null");
+                return null;
+            }
+
+
+            String sql;
+            sql = "SELECT " + column + " FROM " + table;
+            if (extra != "")
+                sql += " " + extra;
+
+            ArrayList<String> res = new ArrayList<String>();
+            Statement st = null;
+            try {
+                st = conn.createStatement();
+                ResultSet rs = null;
+                rs = st.executeQuery(sql);
+
+                while(rs.next()){
+                    String out = rs.getString(column);
+                    res.add(out);
+                    //Log.v(TAG, out.toString());
+                }
+
+                rs.close();
+                st.close();
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            return res;
+        }
+
+        protected static Connection connectToDataBase(){
+            Connection conn = null;
+
+            try {
+                conn = DriverManager. getConnection(url, "parcelexchange", "Mabc0NDkYRf1yVyIfhRd");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            if (conn == null)
+                Log.e("connectToDataBase", "Connection was null");
+
+            return conn;
+        }
+
+    }
 
     private class GetData extends AsyncTask<URL, Integer, Long>{
         protected Long doInBackground(URL... urls) {
